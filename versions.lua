0c420f0ba1081a0f368ef4f7b7599b51b3e19241
Kode Red <dakotarino@gmail.com>
2024-09-01 10:14:56 -0600

    chore: Update sp_lib version in versions.lua

--- versions.lua
+++ versions.lua
@@ -3,6 +3,10 @@ return {
         version = "0.0.3",
         security = "0.0.2"
     },
+    ['sp_lib'] = {
+        version = "0.0.2",
+        security = "0.0.2"
+    },
     ['sp_banking'] = {
         version = "0.0.1",
         security = "0.0.1"
