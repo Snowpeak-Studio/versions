9b263a76b8ef155800c408b5e4bbef2d2c032b71
Kode Red <dakotarino@gmail.com>
2024-09-01 09:56:40 -0600

    chore: Remove unused code and update versions.lua

---
+++ versions.lua
@@ -0,0 +1,22 @@
+return {
+    ['sp_bridge'] = {
+        version = "0.0.3",
+        security = "0.0.2"
+    },
+    ['sp_banking'] = {
+        version = "0.0.1",
+        security = "0.0.1"
+    },
+    ['sp_crafting'] = {
+        version = "0.0.1",
+        security = "0.0.1"
+    },
+    ['sp_scenes'] = {
+        version = "0.0.1",
+        security = "0.0.1"
+    },
+    ['sp_interaction'] = {
+        version = "0.0.3",
+        security = "0.0.3"
+    }
+}
