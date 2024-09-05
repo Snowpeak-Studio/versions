2a6b0f4fab1e259abfad14d99966a3597aae1eb3
Kode Red <dakotarino@gmail.com>
2024-09-05 08:08:12 -0600

    chore: Update versions.lua with new module 'sp_crafting'

--- versions.lua
+++ versions.lua
@@ -15,6 +15,10 @@ return {
         version = "0.0.1",
         security = "0.0.1"
     },
+    ['sp_crafting'] = {
+        version = "0.0.1",
+        security = "0.0.1"
+    },
     ['sp_scenes'] = {
         version = "0.0.1",
         security = "0.0.1"
