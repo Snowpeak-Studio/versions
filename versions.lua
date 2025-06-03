7b1935cedc8dc20f6a86258c6d39ff2d244b0f4f
Kode-Red <130188808+Kode-Red@users.noreply.github.com>
2025-06-03 16:43:05 -0600

    chore: Remove unused modules from versions.lua

--- versions.lua
+++ versions.lua
@@ -7,26 +7,6 @@ return {
         version = "0.0.1",
         security = "0.0.1"
     },
-    ['sp_banking'] = {
-        version = "0.0.1",
-        security = "0.0.1"
-    },
-    ['sp_properties'] = {
-        version = "0.0.1",
-        security = "0.0.1"
-    },
-    ['sp_decorations'] = {
-        version = "0.0.1",
-        security = "0.0.1"
-    },
-    ['sp_crafting'] = {
-        version = "0.0.1",
-        security = "0.0.1"
-    },
-    ['sp_scenes'] = {
-        version = "0.0.1",
-        security = "0.0.1"
-    },
     ['sp_weather'] = {
         version = "0.0.1",
         security = "0.0.1"
