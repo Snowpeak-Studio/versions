52637f1afa301b92957a7f9841af013b5ecaf580
Kode-Red <130188808+Kode-Red@users.noreply.github.com>
2025-09-08 22:59:34 -0600

    chore: Update 'sp_core' to version 1.0.3 and 'sp_music' to version 1.0.2 in versions.lua

--- versions.lua
+++ versions.lua
@@ -1,15 +1,15 @@
 return {
     ['sp_core'] = {
-        version = "1.0.2",
-        security = "1.0.2"
+        version = "1.0.3",
+        security = "1.0.3"
     },
     ['sp_farming'] = {
         version = "0.0.1",
         security = "0.0.1"
     },
     ['sp_music'] = {
-        version = "1.0.0",
-        security = "1.0.0"
+        version = "1.0.2",
+        security = "1.0.2"
     },
     ['sp_chairs'] = {
         version = "0.0.4",
